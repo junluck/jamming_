04fff9f7516e036fa250d24bc7d9576dac8c7c58
junluck <junaindavidsvickerman@gmail.com>
2024-11-11 14:30:49 +0200

    update responsiveness

--- src/Components/HomePage/homepage.js
+++ src/Components/HomePage/homepage.js
@@ -6,23 +6,25 @@ import { Outlet } from "react-router-dom";
 //function component that is the homepage that consists of the buttons title and different vector artwork 
 function Homepage(){
     return (
-        <div className="parentHomeDiv">
-            <Outlet />
-           <div className="stageAndArtist">
-                <img src="guitarist.svg" className="guitarist"/>
-                <img src="pianist.svg" className="pianist"/>
-                <img src="singer.svg" className="singer"/>
-                <img src="drummer.svg" className="drummer"/>
-                <img src="women.svg" className="women"/>
-                <img src="stage.svg" className="stage"/>
-                <img src="stage.svg" className="stageTwo"/>
-                <img src="ShadowCircle.svg" className="shadowCircle"/>
-                <img src="ShadowCircle.svg" className="shadowCircleTwo"/>
-            </div>
-            <div className="linkToSpotify">
-                <LinkToSpotify />
-            </div>
+        <div className="parentDiv">
+            <div className="parentHomeDiv">
+                <Outlet />
+            <div className="stageAndArtist">
+                    <img src="guitarist.svg" className="guitarist"/>
+                    <img src="pianist.svg" className="pianist"/>
+                    <img src="singer.svg" className="singer"/>
+                    <img src="drummer.svg" className="drummer"/>
+                    <img src="women.svg" className="women"/>
+                    <img src="stage.svg" className="stage"/>
+                    <img src="stage.svg" className="stageTwo"/>
+                    <img src="ShadowCircle.svg" className="shadowCircle"/>
+                    <img src="ShadowCircle.svg" className="shadowCircleTwo"/>
+                </div>
+                <div className="linkToSpotify">
+                    <LinkToSpotify />
+                </div>
         </div>
+    </div>
     )
 }
 
